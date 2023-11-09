ea9193ab6022f36b7be7f9bd37a0889a60b01ff2
Diego Gomez <gomedie1974@hotmail.com>
2023-11-09 08:47:29 -0300

    preentrega 3

--- control_empleados/urls.py
+++ control_empleados/urls.py
@@ -3,6 +3,5 @@ from control_empleados.views import listar_empleados
 
 urlpatterns = [
     path('empleados/', listar_empleados),
-    path("empleados/", listar_empleados, name="listar_empleados"),
-
+    
 ]
