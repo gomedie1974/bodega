da3ca7e00fbba2c14878e9cbe9b413a3521ea172
Diego Gomez <gomedie1974@hotmail.com>
2023-11-15 10:55:58 -0300

    clases

--- control_empleados/urls.py
+++ control_empleados/urls.py
@@ -1,22 +1,44 @@
 from django.urls import path
 from control_empleados.views import (listar_empleados, cargar_bodega, listar_bodegas,
 buscar_bodegas, cargar_empleado, buscar_empleados, eliminar_empleado, eliminar_bodega,
-editar_empleado
-)
+editar_empleado, editar_bodega, EmpleadoListView,EmpleadoCreateView,EmpleadoDeleteView,EmpleadoDetailView,EmpleadoUpdateView, BodegaListView, BodegaCreateView,BodegaDeleteView, BodegaDetailView, BodegaUpdateView
+)   
 
 urlpatterns = [
-    path('empleados/', listar_empleados, name='lista_empleados'),
-    path('alta-empleados/', cargar_empleado, name='cargar_empleado'),
-    path('buscar-empleados/', buscar_empleados, name='buscar_empleados'),
+    #URLS de empleados
+    #path('empleados/', listar_empleados, name='lista_empleados'),
+    #path('alta-empleados/', cargar_empleado, name='cargar_empleado'),
+    #path('buscar-empleados/', buscar_empleados, name='buscar_empleados'),
     #<int:id> es el identificador del empleado a borrar
-    path('eliminar-empleados/<int:id>', eliminar_empleado, name='eliminar_empleado'), 
-    path('editar_empleado/<int:id>', editar_empleado, name='editar_empleado'),
+    #path('eliminar-empleados/<int:id>', eliminar_empleado, name='eliminar_empleado'), 
+    #path('editar_empleado/<int:id>', editar_empleado, name='editar_empleado'),
+    #EMPLEADOS
+    path('empleados/', EmpleadoListView.as_view(), name='lista_empleados'),
+    path('empleados/<int:pk>', EmpleadoDetailView.as_view(), name='ver_empleados'),
+    path('alta-empleados/', EmpleadoCreateView.as_view(), name='cargar_empleado'),
+    path('editar_empleado/<int:pk>', EmpleadoUpdateView.as_view(), 
+    name='editar_empleado'),
+    path('eliminar-empleados/<int:pk>', EmpleadoDeleteView.as_view(), name='eliminar_empleado'), 
+ 
 
-    path('alta-bodegas/', cargar_bodega, name='cargar_bodega'),
-    path('lista-bodegas/', listar_bodegas, name='listar_bodegas'),
-    path('buscar-bodegas/', buscar_bodegas, name='buscar_bodegas'),
-    path('eliminar_bodega/<int:id>', eliminar_bodega, name='eliminar_bodega'),
+    #URLS de bodegas
+    #path('alta-bodegas/', cargar_bodega, name='cargar_bodega'),
+    #path('lista-bodegas/', listar_bodegas, name='listar_bodegas'),
+    #path('buscar-bodegas/', buscar_bodegas, name='buscar_bodegas'),
+    #path('eliminar_bodega/<int:id>', eliminar_bodega, name='eliminar_bodega'),
+    #path('editar_bodega/<int:id>', editar_bodega, name='editar_bodega'),
 
  
+    path('bodegas/', BodegaListView.as_view(), name='listar_bodegas'),
+    
+    path('bodegas/<int:pk>', BodegaDetailView.as_view(), name='ver_bodegas'),
+
+    path('alta-bodegas/', BodegaCreateView.as_view(), name='cargar_bodega'),
+    
+    path('editar_bodegas/<int:pk>', BodegaUpdateView.as_view(), 
+    name='editar_bodegas'),
+
+    path('eliminar-bodegas/<int:pk>', BodegaDeleteView.as_view(), name='eliminar_bodegas'), 
+
     
 ]
--- control_empleados/views.py
+++ control_empleados/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.db.models import Q
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from control_empleados.forms import BodegaFormulario, EmpleadoFormulario
 from control_empleados.models import Empleado, Bodega
 
@@ -20,16 +21,26 @@ def listar_empleados(request):
     )
     return http_response
 
-def listar_bodegas(request):
-    contexto = {
-         "bodegas": Bodega.objects.all 
-    }
-    http_response = render(
-        request=request,
-        template_name='control_empleados/listar_bodegas.html',
-        context=contexto,
-    )
-    return http_response
+def buscar_empleados(request):
+    if request.method == "POST":
+        data = request.POST
+        busqueda = data["busqueda"]
+        # Filtro simple
+        #bodegas = Bodega.objects.filter(codigo__contains=busqueda)
+        # Ejemplo filtro avanzado
+        empleados = Empleado.objects.filter(
+            Q(nombre__icontains=busqueda) | Q(apellido__contains=busqueda)
+        )
+
+        contexto = {
+            "empleados": empleados,
+        }
+        http_response = render(
+            request=request,
+            template_name='control_empleados/lista_empleados.html',
+            context=contexto,
+        )
+        return http_response
 
 def cargar_empleado(request):
     if request.method == "POST":
@@ -98,9 +109,7 @@ def editar_empleado(request, id):
         template_name='control_empleados/formulario_alta_empleado.html',
         context={'formulario': formulario},
     )
-
-
-    
+  
 def eliminar_empleado(request, id):
     #obtengo el empleados de la BD
     empleado = Empleado.objects.get(id=id)
@@ -111,6 +120,16 @@ def eliminar_empleado(request, id):
         url_exitosa = reverse('lista_empleados')
         return redirect(url_exitosa)
 
+def listar_bodegas(request):
+    contexto = {
+         "bodegas": Bodega.objects.all 
+    }
+    http_response = render(
+        request=request,
+        template_name='control_empleados/listar_bodegas.html',
+        context=contexto,
+    )
+    return http_response
 
 def cargar_bodega(request):
     if request.method == "POST":
@@ -140,8 +159,6 @@ def cargar_bodega(request):
     )
     return http_response
 
-
-
 def buscar_bodegas(request):
     if request.method == "POST":
         data = request.POST
@@ -163,28 +180,6 @@ def buscar_bodegas(request):
         )
         return http_response
 
-def buscar_empleados(request):
-    if request.method == "POST":
-        data = request.POST
-        busqueda = data["busqueda"]
-        # Filtro simple
-        #bodegas = Bodega.objects.filter(codigo__contains=busqueda)
-        # Ejemplo filtro avanzado
-        empleados = Empleado.objects.filter(
-            Q(nombre__icontains=busqueda) | Q(apellido__contains=busqueda)
-        )
-
-        contexto = {
-            "empleados": empleados,
-        }
-        http_response = render(
-            request=request,
-            template_name='control_empleados/lista_empleados.html',
-            context=contexto,
-        )
-        return http_response
-
-
 def eliminar_bodega(request, id):
     bodega = Bodega.objects.get(id=id)
     if request.method == 'POST':
@@ -193,3 +188,78 @@ def eliminar_bodega(request, id):
         url_exitosa = reverse('listar_bodegas')
         return redirect(url_exitosa)
         
+def editar_bodega(request, id):
+    bodega = Bodega.objects.get(id=id)
+    if request.method == "POST":
+        formulario = BodegaFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data  # es un diccionario
+            bodega.nombre = data["nombre"]
+            bodega.codigo = data["codigo"]
+            bodega.save()
+
+            url_exitosa = reverse('listar_bodegas')  
+            return redirect(url_exitosa)
+        
+    
+    else:  # GET
+        inicial = {
+            'nombre' : bodega.nombre,
+            'codigo' : bodega.codigo,
+        }
+        formulario = BodegaFormulario(initial=inicial)
+    return render(
+        request=request,
+        template_name='control_empleados/formulario_alta_bodegas.html',
+        context={'formulario': formulario},
+    )
+
+
+# VISTAS BASADAS EN CLASES 
+#EMPLEADOS
+class EmpleadoListView(ListView):
+    model = Empleado
+    template_name='control_empleados/lista_empleados.html'
+
+class EmpleadoCreateView(CreateView):
+    model = Empleado
+    fields = ('apellido', 'nombre', 'email', 'telefono', 'dni', 'fecha_nacimiento')
+    success_url = reverse_lazy('lista_empleados')
+    
+class EmpleadoDetailView(DetailView):
+    model = Empleado
+    success_url = reverse_lazy('lista_empleados')
+
+class EmpleadoUpdateView(UpdateView):
+    model = Empleado
+    fields = ('apellido', 'nombre', 'email', 'telefono', 'dni', 'fecha_nacimiento')
+    success_url = reverse_lazy('lista_empleados')
+
+class EmpleadoDeleteView(DeleteView):
+    model = Empleado
+    success_url = reverse_lazy('lista_empleados')
+
+
+#BODEGAS
+class BodegaListView(ListView):
+    model = Bodega
+    template_name='control_empleados/listar_bodegas.html'
+
+class BodegaCreateView(CreateView):
+    model = Bodega
+    fields = ('nombre', 'codigo')
+    success_url = reverse_lazy('listar_bodegas')
+    
+class BodegaDetailView(DetailView):
+    model = Bodega
+    success_url = reverse_lazy('listar_bodegas')
+
+class BodegaUpdateView(UpdateView):
+    model = Bodega
+    fields = ('nombre', 'codigo')
+    success_url = reverse_lazy('listar_bodegas')
+
+class BodegaDeleteView(DeleteView):
+    model = Bodega
+    success_url = reverse_lazy('listar_bodegas')
